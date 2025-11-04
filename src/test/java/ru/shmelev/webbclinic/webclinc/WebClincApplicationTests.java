4e80edb1f82c884be3b76ebca2f2a99e6496eeec
Shmelev Savelii <savahits@gmail.com>
2025-11-04 21:33:12 +0300

    Create WebClinic app with Spring Boot

---
+++ src/test/java/ru/shmelev/webbclinic/webclinc/WebClincApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.shmelev.webbclinic.webclinc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebClincApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
