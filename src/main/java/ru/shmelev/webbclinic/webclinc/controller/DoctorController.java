0c4acc46f1b9db201397dce1cce60641d758437c
Shmelev Savelii <savahits@gmail.com>
2025-11-05 13:54:24 +0300

    feat: add mapping for new.html page and post method add

--- src/main/java/ru/shmelev/webbclinic/webclinc/controller/DoctorController.java
+++ src/main/java/ru/shmelev/webbclinic/webclinc/controller/DoctorController.java
@@ -4,7 +4,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import ru.shmelev.webbclinic.webclinc.model.Doctor;
+import ru.shmelev.webbclinic.webclinc.model.enums.Specialization;
 import ru.shmelev.webbclinic.webclinc.repository.DoctorRepository;
 import ru.shmelev.webbclinic.webclinc.service.DoctorService;
 
@@ -23,5 +27,18 @@ public class DoctorController {
     public String doctors(Model model) {
         model.addAttribute("doctors", doctorService.findAll());
         return "doctors/index";
+    }
+
+    @GetMapping("/new")
+    public String newDoctor(Model model) {
+        model.addAttribute("doctor", new Doctor());
+        model.addAttribute("specializations",  Specialization.values());
+        return "doctors/new";
+    }
+
+    @PostMapping
+    public String newDoctor(@ModelAttribute Doctor doctor) {
+        doctorService.save(doctor);
+        return "redirect:/doctors";
     }
 }
