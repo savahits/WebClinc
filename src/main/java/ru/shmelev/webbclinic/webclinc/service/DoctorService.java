e89c972ee602584276a6476bdcf703ea5eeab236
Shmelev Savelii <savahits@gmail.com>
2025-11-05 11:48:03 +0300

    feat: add DoctorService class with findAll method

---
+++ src/main/java/ru/shmelev/webbclinic/webclinc/service/DoctorService.java
@@ -0,0 +1,22 @@
+package ru.shmelev.webbclinic.webclinc.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import ru.shmelev.webbclinic.webclinc.model.Doctor;
+import ru.shmelev.webbclinic.webclinc.repository.DoctorRepository;
+
+import java.util.List;
+
+@Service
+public class DoctorService {
+    private final DoctorRepository doctorRepository;
+
+    @Autowired
+    public DoctorService(DoctorRepository doctorRepository) {
+        this.doctorRepository = doctorRepository;
+    }
+
+    public List<Doctor> findAll() {
+        return doctorRepository.findAll();
+    }
+}
