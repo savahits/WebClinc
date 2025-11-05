da62ffad8f6a20ca53b5c87ffdd23ab4db178f46
Shmelev Savelii <savahits@gmail.com>
2025-11-05 11:44:50 +0300

    feat: add Doctor entity with JPA mapping

---
+++ src/main/java/ru/shmelev/webbclinic/webclinc/model/Doctor.java
@@ -0,0 +1,83 @@
+package ru.shmelev.webbclinic.webclinc.model;
+
+import jakarta.persistence.*;
+
+import ru.shmelev.webbclinic.webclinc.model.enums.Specialization;
+import java.lang.Long;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "doctor")
+
+public class Doctor {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_doctor")
+    private Long id;  // ← Используйте Integer вместо int
+
+    @Column(name = "name", nullable = false, length = 100)
+    private String name;
+
+    @Column(name = "surname", nullable = false, length = 100)
+    private String surname;
+
+    @Column(name = "patronymic", length = 100)
+    private String patronymic;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "specialization")
+    private Specialization specialization;
+
+    // Конструкторы
+    public Doctor() {
+    }
+
+    public Doctor(String name, String surname, String patronymic, Specialization specialization) {
+        this.name = name;
+        this.surname = surname;
+        this.patronymic = patronymic;
+        this.specialization = specialization;
+    }
+
+    // Геттеры и сеттеры
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getPatronymic() {
+        return patronymic;
+    }
+
+    public void setPatronymic(String patronymic) {
+        this.patronymic = patronymic;
+    }
+
+    public Specialization getSpecialization() {
+        return specialization;
+    }
+
+    public void setSpecialization(Specialization specialization) {
+        this.specialization = specialization;
+    }
+}
