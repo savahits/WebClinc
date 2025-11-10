cfbd73376d88048ba367eb93574b0bda6ea88df6
Shmelev Savelii <savahits@gmail.com>
2025-11-10 15:22:58 +0300

    fix: change imports in DoctorController

--- src/main/java/ru/shmelev/webbclinic/webclinc/controller/DoctorController.java
+++ src/main/java/ru/shmelev/webbclinic/webclinc/controller/DoctorController.java
@@ -3,13 +3,9 @@ package ru.shmelev.webbclinic.webclinc.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import ru.shmelev.webbclinic.webclinc.model.Doctor;
+import org.springframework.web.bind.annotation.*;
+import ru.shmelev.webbclinic.webclinc.model.entity.Doctor;
 import ru.shmelev.webbclinic.webclinc.model.enums.Specialization;
-import ru.shmelev.webbclinic.webclinc.repository.DoctorRepository;
 import ru.shmelev.webbclinic.webclinc.service.DoctorService;
 
 @Controller
@@ -39,6 +35,31 @@ public class DoctorController {
     @PostMapping
     public String newDoctor(@ModelAttribute Doctor doctor) {
         doctorService.save(doctor);
+        return "redirect:/doctors";
+    }
+
+    @GetMapping("show/{id}")
+    public String showDoctor(@PathVariable long id, Model model) {
+        model.addAttribute("doctor", doctorService.findById(id));
+        return "doctors/show";
+    }
+
+    @GetMapping("edit/{id}")
+    public String editDoctor(@PathVariable long id, Model model) {
+        model.addAttribute("doctor", doctorService.findById(id));
+        model.addAttribute("specializations",  Specialization.values());
+        return "doctors/edit";
+    }
+
+    @PatchMapping("{id}")
+    public String editDoctor(@PathVariable long id, @ModelAttribute Doctor doctor) {
+        doctorService.update(id, doctor);
+        return "redirect:/doctors";
+    }
+
+    @PostMapping("{id}/delete")
+    public String deleteDoctor(@PathVariable Long id) {
+        doctorService.deleteById(id);
         return "redirect:/doctors";
     }
 }
