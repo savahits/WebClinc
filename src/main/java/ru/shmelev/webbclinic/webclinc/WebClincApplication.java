ceac0583017c74bd50b014b561e7161d26e48241
Shmelev Savelii <savahits@gmail.com>
2025-11-10 15:53:49 +0300

    feat: Add property source

--- src/main/java/ru/shmelev/webbclinic/webclinc/WebClincApplication.java
+++ src/main/java/ru/shmelev/webbclinic/webclinc/WebClincApplication.java
@@ -2,8 +2,10 @@ package ru.shmelev.webbclinic.webclinc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource("classpath:database.properties")
 public class WebClincApplication {
 
     public static void main(String[] args) {
