c360b4efcc94f7398e0b2aaf1b0ccaaabe1b8808
Shmelev Savelii <savahits@gmail.com>
2025-11-10 15:23:19 +0300

    fix: change imports in DoctorRepository.java

--- src/main/java/ru/shmelev/webbclinic/webclinc/repository/DoctorRepository.java
+++ src/main/java/ru/shmelev/webbclinic/webclinc/repository/DoctorRepository.java
@@ -2,7 +2,7 @@ package ru.shmelev.webbclinic.webclinc.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import ru.shmelev.webbclinic.webclinc.model.Doctor;
+import ru.shmelev.webbclinic.webclinc.model.entity.Doctor;
 
 @Repository
 public interface DoctorRepository extends JpaRepository<Doctor,Long> {
