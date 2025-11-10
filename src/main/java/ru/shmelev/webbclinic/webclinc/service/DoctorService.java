dbba904e06ac58caa4684026b7d366894a126147
Shmelev Savelii <savahits@gmail.com>
2025-11-10 15:24:04 +0300

    feat: add findById, update and deleteById methods to DoctorService.java

--- src/main/java/ru/shmelev/webbclinic/webclinc/service/DoctorService.java
+++ src/main/java/ru/shmelev/webbclinic/webclinc/service/DoctorService.java
@@ -2,7 +2,7 @@ package ru.shmelev.webbclinic.webclinc.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import ru.shmelev.webbclinic.webclinc.model.Doctor;
+import ru.shmelev.webbclinic.webclinc.model.entity.Doctor;
 import ru.shmelev.webbclinic.webclinc.repository.DoctorRepository;
 
 import java.util.List;
@@ -20,7 +20,27 @@ public class DoctorService {
         return doctorRepository.findAll();
     }
 
-    public Doctor save(Doctor doctor) {
-        return doctorRepository.save(doctor);
+    public void save(Doctor doctor) {
+         doctorRepository.save(doctor);
+    }
+
+    public Doctor findById(Long id) {
+        return doctorRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Доктор не найден"));
+    }
+    public void update(Long id, Doctor doctor) {
+        Doctor updateDoctor =  doctorRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Доктор не найден"));
+
+        updateDoctor.setName(doctor.getName());
+        updateDoctor.setSurname(doctor.getSurname());
+        updateDoctor.setPatronymic(doctor.getPatronymic());
+        updateDoctor.setSpecialization(doctor.getSpecialization());
+
+        doctorRepository.save(updateDoctor);
+    }
+
+    public void deleteById(Long id) {
+        doctorRepository.deleteById(id);
     }
 }
