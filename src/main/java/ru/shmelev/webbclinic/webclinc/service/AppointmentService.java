5abb850619a4110e50bcddea19a0e9703b5fddea
Shmelev Savelii <savahits@gmail.com>
2025-11-10 18:31:05 +0300

    feat: create AppointmentService with:    - findAll method

---
+++ src/main/java/ru/shmelev/webbclinic/webclinc/service/AppointmentService.java
@@ -0,0 +1,18 @@
+package ru.shmelev.webbclinic.webclinc.service;
+
+import ru.shmelev.webbclinic.webclinc.model.entity.Appointment;
+import ru.shmelev.webbclinic.webclinc.repository.AppointmentRepository;
+
+import java.util.List;
+
+public class AppointmentService{
+    private final AppointmentRepository appointmentRepository;
+
+    public AppointmentService(AppointmentRepository appointmentRepository) {
+        this.appointmentRepository = appointmentRepository;
+    }
+
+    public List<Appointment> findAll() {
+        return appointmentRepository.findAll();
+    }
+}
