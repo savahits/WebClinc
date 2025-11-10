50a1a05bd946fced12e736eb009a4efc660c6c9d
Shmelev Savelii <savahits@gmail.com>
2025-11-10 18:46:54 +0300

    feat: create AppointmentController.java with appointments/index.html mapping

---
+++ src/main/java/ru/shmelev/webbclinic/webclinc/controller/AppointmentController.java
@@ -0,0 +1,26 @@
+package ru.shmelev.webbclinic.webclinc.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import ru.shmelev.webbclinic.webclinc.service.AppointmentService;
+
+@Controller
+@RequestMapping("/appointments")
+public class AppointmentController {
+
+    AppointmentService appointmentService;
+
+    @Autowired
+    public AppointmentController(AppointmentService appointmentService) {
+        this.appointmentService = appointmentService;
+    }
+
+    @GetMapping()
+    public String appointments(Model model) {
+        model.addAttribute(appointmentService.findAll());
+        return "appointments/index";
+    }
+}
