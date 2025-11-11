c17a7da0ed3deee1e247734f0b0039101328d34e
Shmelev Savelii <savahits@gmail.com>
2025-11-11 17:32:00 +0300

    fix: add Service annotation and Autowired

--- src/main/java/ru/shmelev/webbclinic/webclinc/service/AppointmentService.java
+++ src/main/java/ru/shmelev/webbclinic/webclinc/service/AppointmentService.java
@@ -1,15 +1,18 @@
 package ru.shmelev.webbclinic.webclinc.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 import ru.shmelev.webbclinic.webclinc.model.entity.Appointment;
 import ru.shmelev.webbclinic.webclinc.repository.AppointmentRepository;
 
 import java.util.List;
 
-@Component
+@Service
 public class AppointmentService{
     private final AppointmentRepository appointmentRepository;
 
+    @Autowired
     public AppointmentService(AppointmentRepository appointmentRepository) {
         this.appointmentRepository = appointmentRepository;
     }
