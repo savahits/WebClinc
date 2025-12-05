e33554c642751df9c111f29beaa2c955a23d492e
Savelii <savahits@gmail.com>
2025-12-05 11:06:06 +0300

    feat: create enum Role

---
+++ src/main/java/ru/shmelev/webbclinic/webclinc/model/entity/Role.java
@@ -0,0 +1,7 @@
+package ru.shmelev.webbclinic.webclinc.model.entity;
+
+public enum Role {
+    ROLE_SUPER_ADMIN,  // Супер-админ: всё + управление ролями
+    ROLE_ADMIN,        // Админ: CRUD операции
+    ROLE_USER          // Обычный пользователь (если нужно)
+}
